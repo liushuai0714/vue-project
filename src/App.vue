f0cb439acb657926805e8a6cfa3e63a65d8fb441
liushuai0714 <944566598@qq.com>
2023-05-15 13:22:39 +0800

    defineExpose

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2 @click="clickEvent">我是父组件</h2>
+    <Child ref="son"/>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Child from './pages/Child.vue'
+const son = ref(null)
+function clickEvent(){
+  console.log("click here");
+  console.log(son.value.msg);
+  son.value.m1()
+}
+</script>
+
+
---
+++ src/pages/Child.vue
@@ -0,0 +1,18 @@
+<template>
+    <span>我是子组件 --msg:{{ msg }}</span>
+</template>
+
+<script setup>
+import { ref } from "vue"
+const msg = ref("this is a msg from child conponent")
+const m1 = () =>{
+    console.log("this is method m1 at child component");
+    
+}
+//默认不暴露子组件的数据
+//暴露后父组件才可以获取到数据
+defineExpose({
+    msg,
+    m1
+})
+</script>
